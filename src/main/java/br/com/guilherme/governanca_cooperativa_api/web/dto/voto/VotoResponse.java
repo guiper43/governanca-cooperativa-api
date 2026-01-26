fe3011c79284be2cd30ed802095cf19099e8d8ec
guiper43 <87154378+guiper43@users.noreply.github.com>
2026-01-26 13:34:35 -0300

    refactor(sservice): adequar voto response com campo id em contrato

--- src/main/java/br/com/guilherme/governanca_cooperativa_api/web/dto/voto/VotoResponse.java
+++ src/main/java/br/com/guilherme/governanca_cooperativa_api/web/dto/voto/VotoResponse.java
@@ -5,6 +5,7 @@ import br.com.guilherme.governanca_cooperativa_api.domain.enums.VotoEscolha;
 import java.util.UUID;
 
 public record VotoResponse(
+    UUID id,
     UUID pautaId,
     String associadoId,
     VotoEscolha votoEscolha
