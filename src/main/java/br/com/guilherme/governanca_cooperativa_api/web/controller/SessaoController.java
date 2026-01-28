18cfea2ebf3ffb540370c4e060ea6cb0766bc06a
guiper43 <87154378+guiper43@users.noreply.github.com>
2026-01-28 16:44:28 -0300

    fix(controller): ajustar importacoes para o funcionamento correto da class

--- src/main/java/br/com/guilherme/governanca_cooperativa_api/web/controller/SessaoController.java
+++ src/main/java/br/com/guilherme/governanca_cooperativa_api/web/controller/SessaoController.java
@@ -7,12 +7,8 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
@@ -24,9 +20,9 @@ public class SessaoController {
     private final SessaoService service;
 
     @PostMapping
-    public ResponseEntity<SessaoResponse> abrir(@PathVariable UUID pautaId, @Valid @RequestBody(required = falser) SessaoRequest request) {
+    public ResponseEntity<SessaoResponse> abrir(@PathVariable UUID pautaId, @Valid @RequestBody(required = false) SessaoRequest request) {
         Integer duracao = request.duracaoMinutos() == null ? 1 : request.duracaoMinutos();
-var response = service.abrir(pautaId, duracao);
-return ResponseEntity.status(HttpStatus.CREATED).body(response);
+        var response = service.abrir(pautaId, duracao);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 }
